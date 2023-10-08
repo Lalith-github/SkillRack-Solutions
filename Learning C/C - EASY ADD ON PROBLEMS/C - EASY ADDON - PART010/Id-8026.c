4481da8745636f15ce33eb63487dedf867c198d3
USHA NANDHINI <gushanandhini2004@gmail.com>
2023-10-08 17:58:02 +0530

    Update Id-8026.c

--- Learning C/C - EASY ADD ON PROBLEMS/C - EASY ADDON - PART010/Id-8026.c
+++ Learning C/C - EASY ADD ON PROBLEMS/C - EASY ADDON - PART010/Id-8026.c
@@ -2,21 +2,22 @@
 
 int main() {
     int N;
+    printf("Input:\n");
     scanf("%d", &N);
 
     int matrix[N][N];
 
     // Input the matrix
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             scanf("%d", &matrix[i][j]);
         }
     }
 
-    // Print the modified matrix
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
-            if (j >= N - i) {
+    // Output the modified matrix
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (j >= i) {
                 printf("%d ", matrix[i][j]);
             } else {
                 printf("* ");
