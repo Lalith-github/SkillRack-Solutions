e0a5ddeeb476131e1501681b8a788c604b941c39
Lalith-github <152771199+Lalith-github@users.noreply.github.com>
2024-06-04 20:39:28 +0530

    Update Id-2582.c

--- Learning C/C - EASY PROBLEMS/C - EASY - PART003/Id-2582.c
+++ Learning C/C - EASY PROBLEMS/C - EASY - PART003/Id-2582.c
@@ -1,27 +1,27 @@
-// #include <stdio.h>
-// #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// int main()
-// {
-//     int i = 0, count = 0, o[99], i, x, y, t = 0;
-//     char c = ' ';
-//     while (c == ' ')
-//     {
-//         scanf("%d%c", &o[i], &c);
-//         i++;
-//         count++;
-//     }
-//     scanf("%d\n%d", &x, &y);
-//     for (i = 0; i < count; i++)
-//     {
-//         if (o[i] % 2 == 0)
-//         {
-//             t -= y;
-//         }
-//         else
-//         {
-//             t += x;
-//         }
-//     }
-//     printf("%d", t);
-// }
+int main()
+{
+    int i = 0, count = 0, o[99], i, x, y, t = 0;
+    char c = ' ';
+    while (c == ' ')
+    {
+        scanf("%d%c", &o[i], &c);
+        i++;
+        count++;
+    }
+    scanf("%d\n%d", &x, &y);
+    for (i = 0; i < count; i++)
+    {
+        if (o[i] % 2 == 0)
+        {
+            t -= y;
+        }
+        else
+        {
+            t += x;
+        }
+    }
+    printf("%d", t);
+}
